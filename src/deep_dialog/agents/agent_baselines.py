c3a6f050eeeb7a320580bf65b8dee9f99d06d5ad
Valentin Malykh <madrugado@yandex.ru>
2017-03-20 16:46:54 +0300

    ENH: code style

--- src/deep_dialog/agents/agent_baselines.py
+++ src/deep_dialog/agents/agent_baselines.py
@@ -4,8 +4,10 @@ Created on May 25, 2016
 @author: xiul, t-zalipt
 """
 
-import copy, random
-from deep_dialog import dialog_config
+import copy
+import random
+
+from src.deep_dialog import dialog_config
 from .agent import Agent
 
 
@@ -22,7 +24,7 @@ class InformAgent(Agent):
 
     def state_to_action(self, state):
         """ Run current policy on state and produce an action """
-        
+
         self.state['turn'] += 2
         if self.current_slot_id < len(list(self.slot_set.keys())):
             slot = list(self.slot_set.keys())[self.current_slot_id]
@@ -34,14 +36,14 @@ class InformAgent(Agent):
             act_slot_response['request_slots'] = {}
             act_slot_response['turn'] = self.state['turn']
         else:
-            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {}, 'turn': self.state['turn']}
+            act_slot_response = {'diaact': "thanks", 'inform_slots': {},
+                                 'request_slots': {}, 'turn': self.state['turn']}
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
-
 
 
 class RequestAllAgent(Agent):
     """ A simple agent to test the system. This agent should simply request all the slots and then issue: thanks(). """
-    
+
     def initialize_episode(self):
         self.state = {}
         self.state['diaact'] = ''
@@ -52,7 +54,7 @@ class RequestAllAgent(Agent):
 
     def state_to_action(self, state):
         """ Run current policy on state and produce an action """
-        
+
         self.state['turn'] += 2
         if self.current_slot_id < len(dialog_config.sys_request_slots):
             slot = dialog_config.sys_request_slots[self.current_slot_id]
@@ -64,9 +66,9 @@ class RequestAllAgent(Agent):
             act_slot_response['request_slots'] = {slot: "PLACEHOLDER"}
             act_slot_response['turn'] = self.state['turn']
         else:
-            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {}, 'turn': self.state['turn']}
+            act_slot_response = {'diaact': "thanks", 'inform_slots': {},
+                                 'request_slots': {}, 'turn': self.state['turn']}
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
-
 
 
 class RandomAgent(Agent):
@@ -79,19 +81,18 @@ class RandomAgent(Agent):
         self.state['request_slots'] = {}
         self.state['turn'] = -1
 
-
     def state_to_action(self, state):
         """ Run current policy on state and produce an action """
-        
+
         self.state['turn'] += 2
         act_slot_response = copy.deepcopy(random.choice(dialog_config.feasible_actions))
         act_slot_response['turn'] = self.state['turn']
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
 
 
-
 class EchoAgent(Agent):
-    """ A simple agent that informs all requested slots, then issues inform(taskcomplete) when the user stops making requests. """
+    """ A simple agent that informs all requested slots,
+    then issues inform(taskcomplete) when the user stops making requests. """
 
     def initialize_episode(self):
         self.state = {}
@@ -100,11 +101,10 @@ class EchoAgent(Agent):
         self.state['request_slots'] = {}
         self.state['turn'] = -1
 
-
     def state_to_action(self, state):
         """ Run current policy on state and produce an action """
         user_action = state['user_action']
-        
+
         self.state['turn'] += 2
         act_slot_response = {}
         act_slot_response['inform_slots'] = {}
@@ -126,8 +126,9 @@ class EchoAgent(Agent):
 
 
 class RequestBasicsAgent(Agent):
-    """ A simple agent to test the system. This agent should simply request all the basic slots and then issue: thanks(). """
-    
+    """ A simple agent to test the system. This agent should simply request
+     all the basic slots and then issue: thanks(). """
+
     def initialize_episode(self):
         self.state = {}
         self.state['diaact'] = 'UNK'
@@ -140,7 +141,7 @@ class RequestBasicsAgent(Agent):
 
     def state_to_action(self, state):
         """ Run current policy on state and produce an action """
-        
+
         self.state['turn'] += 2
         if self.current_slot_id < len(self.request_set):
             slot = self.request_set[self.current_slot_id]
@@ -152,11 +153,12 @@ class RequestBasicsAgent(Agent):
             act_slot_response['request_slots'] = {slot: "UNK"}
             act_slot_response['turn'] = self.state['turn']
         elif self.phase == 0:
-            act_slot_response = {'diaact': "inform", 'inform_slots': {'taskcomplete': "PLACEHOLDER"}, 'request_slots': {}, 'turn':self.state['turn']}
+            act_slot_response = {'diaact': "inform", 'inform_slots': {'taskcomplete': "PLACEHOLDER"},
+                                 'request_slots': {}, 'turn': self.state['turn']}
             self.phase += 1
         elif self.phase == 1:
-            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {}, 'turn': self.state['turn']}
+            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {},
+                                 'turn': self.state['turn']}
         else:
             raise Exception("THIS SHOULD NOT BE POSSIBLE (AGENT CALLED IN UNANTICIPATED WAY)")
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
-
--- src/deep_dialog/agents/agent_dqn.py
+++ src/deep_dialog/agents/agent_dqn.py
@@ -1,4 +1,4 @@
-'''
+"""
 Created on Jun 18, 2016
 
 An DQN Agent
@@ -11,18 +11,18 @@ Command: python .\run.py --agt 9 --usr 1 --max_turn 40 --movie_kb_path .\deep_di
 
 
 @author: xiul
-'''
+"""
 
-
-import random, copy, json
+import copy
+import json
 import pickle
+import random
+
 import numpy as np
 
-from deep_dialog import dialog_config
-
+from src.deep_dialog import dialog_config
+from src.deep_dialog.qlearning import DQN
 from .agent import Agent
-from deep_dialog.qlearning import DQN
-
 
 
 class AgentDQN(Agent):
@@ -32,74 +32,71 @@ class AgentDQN(Agent):
         self.slot_set = slot_set
         self.act_cardinality = len(list(act_set.keys()))
         self.slot_cardinality = len(list(slot_set.keys()))
-        
+
         self.feasible_actions = dialog_config.feasible_actions
         self.num_actions = len(self.feasible_actions)
-        
+
         self.epsilon = params['epsilon']
         self.agent_run_mode = params['agent_run_mode']
         self.agent_act_level = params['agent_act_level']
-        self.experience_replay_pool = [] #experience replay pool <s_t, a_t, r_t, s_t+1>
-        
+        self.experience_replay_pool = []  # experience replay pool <s_t, a_t, r_t, s_t+1>
+
         self.experience_replay_pool_size = params.get('experience_replay_pool_size', 1000)
         self.hidden_size = params.get('dqn_hidden_size', 60)
         self.gamma = params.get('gamma', 0.9)
         self.predict_mode = params.get('predict_mode', False)
         self.warm_start = params.get('warm_start', 0)
-        
+
         self.max_turn = params['max_turn'] + 4
         self.state_dimension = 2 * self.act_cardinality + 7 * self.slot_cardinality + 3 + self.max_turn
-        
+
         self.dqn = DQN(self.state_dimension, self.hidden_size, self.num_actions)
         self.clone_dqn = copy.deepcopy(self.dqn)
-        
+
         self.cur_bellman_err = 0
-                
+
         # Prediction Mode: load trained DQN model
-        if params['trained_model_path'] != None:
+        if params['trained_model_path'] is not None:
             self.dqn.model = copy.deepcopy(self.load_trained_DQN(params['trained_model_path']))
             self.clone_dqn = copy.deepcopy(self.dqn)
             self.predict_mode = True
             self.warm_start = 2
-            
-            
+
     def initialize_episode(self):
         """ Initialize a new episode. This function is called every time a new episode is run. """
-        
+
         self.current_slot_id = 0
         self.phase = 0
         self.request_set = ['moviename', 'starttime', 'city', 'date', 'theater', 'numberofpeople']
-    
-    
+
     def state_to_action(self, state):
         """ DQN: Input state, output action """
-        
+
         self.representation = self.prepare_state_representation(state)
         self.action = self.run_policy(self.representation)
         act_slot_response = copy.deepcopy(self.feasible_actions[self.action])
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
-        
-    
+
     def prepare_state_representation(self, state):
         """ Create the representation for each state """
-        
+
         user_action = state['user_action']
         current_slots = state['current_slots']
         kb_results_dict = state['kb_results_dict']
         agent_last = state['agent_action']
-        
+
         ########################################################################
         #   Create one-hot of acts to represent the current user action
         ########################################################################
-        user_act_rep =  np.zeros((1, self.act_cardinality))
-        user_act_rep[0,self.act_set[user_action['diaact']]] = 1.0
+        user_act_rep = np.zeros((1, self.act_cardinality))
+        user_act_rep[0, self.act_set[user_action['diaact']]] = 1.0
 
         ########################################################################
         #     Create bag of inform slots representation to represent the current user action
         ########################################################################
         user_inform_slots_rep = np.zeros((1, self.slot_cardinality))
         for slot in user_action['inform_slots'].keys():
-            user_inform_slots_rep[0,self.slot_set[slot]] = 1.0
+            user_inform_slots_rep[0, self.slot_set[slot]] = 1.0
 
         ########################################################################
         #   Create bag of request slots representation to represent the current user action
@@ -118,7 +115,7 @@ class AgentDQN(Agent):
         ########################################################################
         #   Encode last agent act
         ########################################################################
-        agent_act_rep = np.zeros((1,self.act_cardinality))
+        agent_act_rep = np.zeros((1, self.act_cardinality))
         if agent_last:
             agent_act_rep[0, self.act_set[agent_last['diaact']]] = 1.0
 
@@ -128,7 +125,7 @@ class AgentDQN(Agent):
         agent_inform_slots_rep = np.zeros((1, self.slot_cardinality))
         if agent_last:
             for slot in agent_last['inform_slots'].keys():
-                agent_inform_slots_rep[0,self.slot_set[slot]] = 1.0
+                agent_inform_slots_rep[0, self.slot_set[slot]] = 1.0
 
         ########################################################################
         #   Encode last agent request slots
@@ -136,9 +133,9 @@ class AgentDQN(Agent):
         agent_request_slots_rep = np.zeros((1, self.slot_cardinality))
         if agent_last:
             for slot in agent_last['request_slots'].keys():
-                agent_request_slots_rep[0,self.slot_set[slot]] = 1.0
-        
-        turn_rep = np.zeros((1,1)) + state['turn'] / 10.
+                agent_request_slots_rep[0, self.slot_set[slot]] = 1.0
+
+        turn_rep = np.zeros((1, 1)) + state['turn'] / 10.
 
         ########################################################################
         #  One-hot representation of the turn count?
@@ -157,17 +154,20 @@ class AgentDQN(Agent):
         ########################################################################
         #   Representation of KB results (binary)
         ########################################################################
-        kb_binary_rep = np.zeros((1, self.slot_cardinality + 1)) + np.sum( kb_results_dict['matching_all_constraints'] > 0.)
+        kb_binary_rep = np.zeros((1, self.slot_cardinality + 1)) + np.sum(
+            kb_results_dict['matching_all_constraints'] > 0.)
         for slot in kb_results_dict:
             if slot in self.slot_set:
-                kb_binary_rep[0, self.slot_set[slot]] = np.sum( kb_results_dict[slot] > 0.)
+                kb_binary_rep[0, self.slot_set[slot]] = np.sum(kb_results_dict[slot] > 0.)
 
-        self.final_representation = np.hstack([user_act_rep, user_inform_slots_rep, user_request_slots_rep, agent_act_rep, agent_inform_slots_rep, agent_request_slots_rep, current_slots_rep, turn_rep, turn_onehot_rep, kb_binary_rep, kb_count_rep])
+        self.final_representation = np.hstack(
+            [user_act_rep, user_inform_slots_rep, user_request_slots_rep, agent_act_rep, agent_inform_slots_rep,
+             agent_request_slots_rep, current_slots_rep, turn_rep, turn_onehot_rep, kb_binary_rep, kb_count_rep])
         return self.final_representation
-      
+
     def run_policy(self, representation):
         """ epsilon-greedy policy """
-        
+
         if random.random() < self.epsilon:
             return random.randint(0, self.num_actions - 1)
         else:
@@ -177,10 +177,10 @@ class AgentDQN(Agent):
                 return self.rule_policy()
             else:
                 return self.dqn.predict(representation, {}, predict_model=True)
-    
+
     def rule_policy(self):
         """ Rule Policy """
-        
+
         if self.current_slot_id < len(self.request_set):
             slot = self.request_set[self.current_slot_id]
             self.current_slot_id += 1
@@ -190,77 +190,77 @@ class AgentDQN(Agent):
             act_slot_response['inform_slots'] = {}
             act_slot_response['request_slots'] = {slot: "UNK"}
         elif self.phase == 0:
-            act_slot_response = {'diaact': "inform", 'inform_slots': {'taskcomplete': "PLACEHOLDER"}, 'request_slots': {} }
+            act_slot_response = {'diaact': "inform", 'inform_slots': {'taskcomplete': "PLACEHOLDER"},
+                                 'request_slots': {}}
             self.phase += 1
         elif self.phase == 1:
-            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {} }
-                
+            act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {}}
+
         return self.action_index(act_slot_response)
-    
+
     def action_index(self, act_slot_response):
         """ Return the index of action """
-        
+
         for (i, action) in enumerate(self.feasible_actions):
             if act_slot_response == action:
                 return i
-        print(act_slot_response)
-        raise Exception("action index not found")
+        else:
+            print(act_slot_response)
+            raise Exception("action index not found")
         return None
-    
-    
+
     def register_experience_replay_tuple(self, s_t, a_t, reward, s_tplus1, episode_over):
         """ Register feedback from the environment, to be stored as future training data """
-        
+
         state_t_rep = self.prepare_state_representation(s_t)
         action_t = self.action
         reward_t = reward
         state_tplus1_rep = self.prepare_state_representation(s_tplus1)
         training_example = (state_t_rep, action_t, reward_t, state_tplus1_rep, episode_over)
-        
-        if self.predict_mode == False: # Training Mode
+
+        if self.predict_mode == False:  # Training Mode
             if self.warm_start == 1:
                 self.experience_replay_pool.append(training_example)
-        else: # Prediction Mode
+        else:  # Prediction Mode
             self.experience_replay_pool.append(training_example)
-    
+
     def train(self, batch_size=1, num_batches=100):
         """ Train DQN with experience replay """
-        
+
         for iter_batch in range(num_batches):
             self.cur_bellman_err = 0
-            for iter in range(len(self.experience_replay_pool)/(batch_size)):
+            for iter in range(len(self.experience_replay_pool) / (batch_size)):
                 batch = [random.choice(self.experience_replay_pool) for i in range(batch_size)]
                 batch_struct = self.dqn.singleBatch(batch, {'gamma': self.gamma}, self.clone_dqn)
                 self.cur_bellman_err += batch_struct['cost']['total_cost']
-            
-            print("cur bellman err %.4f, experience replay pool %s" % (float(self.cur_bellman_err)/len(self.experience_replay_pool), len(self.experience_replay_pool)))
-            
-            
+
+            print("cur bellman err %.4f, experience replay pool %s" % (
+                float(self.cur_bellman_err) / len(self.experience_replay_pool), len(self.experience_replay_pool)))
+
     ################################################################################
     #    Debug Functions
     ################################################################################
     def save_experience_replay_to_file(self, path):
         """ Save the experience replay pool to a file """
-        
+
         try:
             with open(path, "wb") as f:
                 pickle.dump(self.experience_replay_pool, f)
             print('saved model in %s' % path)
         except Exception as e:
             print('Error: Writing model fails: %s' % path)
-            print(e)         
-    
+            print(e)
+
     def load_experience_replay_from_file(self, path):
         """ Load the experience replay pool from a file"""
-        
+
         self.experience_replay_pool = pickle.load(open(path, 'rb'))
-    
-             
+
     def load_trained_DQN(self, path):
         """ Load the trained DQN from a file """
-        
+
         trained_file = pickle.load(open(path, 'rb'))
         model = trained_file['model']
-        
+
         print("trained DQN Parameters:", json.dumps(trained_file['params'], indent=2))
         return model
