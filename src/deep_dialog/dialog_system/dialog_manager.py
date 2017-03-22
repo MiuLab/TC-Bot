7a36f60dea468458f45d672e209d6fa5e0419d40
Valentin Malykh <madrugado@yandex.ru>
2017-03-22 17:13:47 +0300

    ENH: code style

--- src/deep_dialog/dialog_system/dialog_manager.py
+++ src/deep_dialog/dialog_system/dialog_manager.py
@@ -6,12 +6,12 @@ Created on May 17, 2016
 
 import json
 from . import StateTracker
-from deep_dialog import dialog_config
+from src.deep_dialog import dialog_config
 
 
 class DialogManager:
     """ A dialog manager to mediate the interaction between an agent and a customer """
-    
+
     def __init__(self, agent, user, act_set, slot_set, movie_dictionary):
         self.agent = agent
         self.user = user
@@ -24,109 +24,117 @@ class DialogManager:
 
     def initialize_episode(self):
         """ Refresh state for new dialog """
-        
+
         self.reward = 0
         self.episode_over = False
         self.state_tracker.initialize_episode()
         self.user_action = self.user.initialize_episode()
-        self.state_tracker.update(user_action = self.user_action)
-        
+        self.state_tracker.update(user_action=self.user_action)
+
         if dialog_config.run_mode < 3:
-            print ("New episode, user goal:")
+            print("New episode, user goal:")
             print(json.dumps(self.user.goal, indent=2))
-        self.print_function(user_action = self.user_action)
-            
+        self.print_function(user_action=self.user_action)
+
         self.agent.initialize_episode()
 
     def next_turn(self, record_training_data=True):
         """ This function initiates each subsequent exchange between agent and user (agent first) """
-        
+
         ########################################################################
         #   CALL AGENT TO TAKE HER TURN
         ########################################################################
         self.state = self.state_tracker.get_state_for_agent()
         self.agent_action = self.agent.state_to_action(self.state)
-        
+
         ########################################################################
         #   Register AGENT action with the state_tracker
         ########################################################################
         self.state_tracker.update(agent_action=self.agent_action)
-        
-        self.agent.add_nl_to_action(self.agent_action) # add NL to Agent Dia_Act
-        self.print_function(agent_action = self.agent_action['act_slot_response'])
-        
+
+        self.agent.add_nl_to_action(self.agent_action)  # add NL to Agent Dia_Act
+        self.print_function(agent_action=self.agent_action['act_slot_response'])
+
         ########################################################################
         #   CALL USER TO TAKE HER TURN
         ########################################################################
         self.sys_action = self.state_tracker.dialog_history_dictionaries()[-1]
         self.user_action, self.episode_over, dialog_status = self.user.next(self.sys_action)
         self.reward = self.reward_function(dialog_status)
-        
+
         ########################################################################
         #   Update state tracker with latest user action
         ########################################################################
         if self.episode_over != True:
-            self.state_tracker.update(user_action = self.user_action)
-            self.print_function(user_action = self.user_action)
+            self.state_tracker.update(user_action=self.user_action)
+            self.print_function(user_action=self.user_action)
 
         ########################################################################
         #  Inform agent of the outcome for this timestep (s_t, a_t, r, s_{t+1}, episode_over)
         ########################################################################
         if record_training_data:
-            self.agent.register_experience_replay_tuple(self.state, self.agent_action, self.reward, self.state_tracker.get_state_for_agent(), self.episode_over)
-        
-        return (self.episode_over, self.reward)
+            self.agent.register_experience_replay_tuple(self.state, self.agent_action, self.reward,
+                                                        self.state_tracker.get_state_for_agent(), self.episode_over)
 
-    
+        return self.episode_over, self.reward
+
     def reward_function(self, dialog_status):
         """ Reward Function 1: a reward function based on the dialog_status """
         if dialog_status == dialog_config.FAILED_DIALOG:
-            reward = -self.user.max_turn #10
+            reward = -self.user.max_turn  # 10
         elif dialog_status == dialog_config.SUCCESS_DIALOG:
-            reward = 2*self.user.max_turn #20
+            reward = 2 * self.user.max_turn  # 20
         else:
             reward = -1
         return reward
-    
+
     def reward_function_without_penalty(self, dialog_status):
         """ Reward Function 2: a reward function without penalty on per turn and failure dialog """
         if dialog_status == dialog_config.FAILED_DIALOG:
             reward = 0
         elif dialog_status == dialog_config.SUCCESS_DIALOG:
-            reward = 2*self.user.max_turn
+            reward = 2 * self.user.max_turn
         else:
             reward = 0
         return reward
-    
-    
+
     def print_function(self, agent_action=None, user_action=None):
         """ Print Function """
-            
+
         if agent_action:
             if dialog_config.run_mode == 0:
                 if self.agent.__class__.__name__ != 'AgentCmd':
                     print("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
             elif dialog_config.run_mode == 1:
                 if self.agent.__class__.__name__ != 'AgentCmd':
-                    print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
-            elif dialog_config.run_mode == 2: # debug mode
-                print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
+                    print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (
+                    agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'],
+                    agent_action['request_slots']))
+            elif dialog_config.run_mode == 2:  # debug mode
+                print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (
+                agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'],
+                agent_action['request_slots']))
                 print("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
-            
+
             if dialog_config.auto_suggest == 1:
-                print('(Suggested Values: %s)' % (self.state_tracker.get_suggest_slots_values(agent_action['request_slots'])))
+                print('(Suggested Values: %s)'
+                      % (self.state_tracker.get_suggest_slots_values(agent_action['request_slots'])))
         elif user_action:
             if dialog_config.run_mode == 0:
                 print("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
-            elif dialog_config.run_mode == 1: 
-                print("Turn %s usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
-            elif dialog_config.run_mode == 2: # debug mode, show both
-                print("Turn %d usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
+            elif dialog_config.run_mode == 1:
+                print("Turn %s usr: %s, inform_slots: %s, request_slots: %s"
+                      % (user_action['turn'], user_action['diaact'],
+                         user_action['inform_slots'], user_action['request_slots']))
+            elif dialog_config.run_mode == 2:  # debug mode, show both
+                print("Turn %d usr: %s, inform_slots: %s, request_slots: %s"
+                      % (user_action['turn'], user_action['diaact'],
+                         user_action['inform_slots'], user_action['request_slots']))
                 print("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
-            
-            if self.agent.__class__.__name__ == 'AgentCmd': # command line agent
+
+            if self.agent.__class__.__name__ == 'AgentCmd':  # command line agent
                 user_request_slots = user_action['request_slots']
-                if 'ticket'in user_request_slots.keys(): del user_request_slots['ticket']
+                if 'ticket' in user_request_slots.keys(): del user_request_slots['ticket']
                 if len(user_request_slots) > 0:
                     possible_values = self.state_tracker.get_suggest_slots_values(user_action['request_slots'])
                     for slot in possible_values.keys():
@@ -136,5 +144,4 @@ class DialogManager:
                             print('(Suggested Values: there is no available %s)' % (slot))
                 else:
                     kb_results = self.state_tracker.get_current_kb_results()
-                    print ('(Number of movies in KB satisfying current constraints: %s)' % len(kb_results))
-                    
+                    print('(Number of movies in KB satisfying current constraints: %s)' % len(kb_results))
--- src/deep_dialog/dialog_system/state_tracker.py
+++ src/deep_dialog/dialog_system/state_tracker.py
@@ -6,7 +6,7 @@ state tracker
 @author: xiul, t-zalipt
 """
 
-from . import KBHelper
+from .kb_helper import KBHelper
 import numpy as np
 import copy
 
@@ -35,108 +35,111 @@ class StateTracker:
         self.history_vectors = None
         self.history_dictionaries = None
         self.current_slots = None
-        self.action_dimension = 10      # TODO REPLACE WITH REAL VALUE
-        self.kb_result_dimension = 10   # TODO  REPLACE WITH REAL VALUE
+        self.action_dimension = 10  # TODO REPLACE WITH REAL VALUE
+        self.kb_result_dimension = 10  # TODO  REPLACE WITH REAL VALUE
         self.turn_count = 0
         self.kb_helper = KBHelper(movie_dictionary)
-        
 
     def initialize_episode(self):
         """ Initialize a new episode (dialog), flush the current state and tracked slots """
-        
+
         self.action_dimension = 10
         self.history_vectors = np.zeros((1, self.action_dimension))
         self.history_dictionaries = []
         self.turn_count = 0
         self.current_slots = {}
-        
+
         self.current_slots['inform_slots'] = {}
         self.current_slots['request_slots'] = {}
         self.current_slots['proposed_slots'] = {}
         self.current_slots['agent_request_slots'] = {}
 
-
     def dialog_history_vectors(self):
         """ Return the dialog history (both user and agent actions) in vector representation """
         return self.history_vectors
 
-
     def dialog_history_dictionaries(self):
         """  Return the dictionary representation of the dialog history (includes values) """
         return self.history_dictionaries
-
 
     def kb_results_for_state(self):
         """ Return the information about the database results based on the currently informed slots """
         ########################################################################
         # TODO Calculate results based on current informed slots
         ########################################################################
-        kb_results = self.kb_helper.database_results_for_agent(self.current_slots) # replace this with something less ridiculous
+        kb_results = self.kb_helper.database_results_for_agent(
+            self.current_slots)  # replace this with something less ridiculous
         # TODO turn results into vector (from dictionary)
         results = np.zeros((0, self.kb_result_dimension))
         return results
-        
 
     def get_state_for_agent(self):
         """ Get the state representatons to send to agent """
-        #state = {'user_action': self.history_dictionaries[-1], 'current_slots': self.current_slots, 'kb_results': self.kb_results_for_state()}
-        state = {'user_action': self.history_dictionaries[-1], 'current_slots': self.current_slots, #'kb_results': self.kb_results_for_state(), 
-                 'kb_results_dict':self.kb_helper.database_results_for_agent(self.current_slots), 'turn': self.turn_count, 'history': self.history_dictionaries, 
+        # state = {'user_action': self.history_dictionaries[-1], 'current_slots': self.current_slots,
+        #          'kb_results': self.kb_results_for_state()}
+        state = {'user_action': self.history_dictionaries[-1], 'current_slots': self.current_slots,
+                 # 'kb_results': self.kb_results_for_state(),
+                 'kb_results_dict': self.kb_helper.database_results_for_agent(self.current_slots),
+                 'turn': self.turn_count, 'history': self.history_dictionaries,
                  'agent_action': self.history_dictionaries[-2] if len(self.history_dictionaries) > 1 else None}
         return copy.deepcopy(state)
-    
+
     def get_suggest_slots_values(self, request_slots):
         """ Get the suggested values for request slots """
-        
+
         suggest_slot_vals = {}
-        if len(request_slots) > 0: 
+        if len(request_slots) > 0:
             suggest_slot_vals = self.kb_helper.suggest_slot_values(request_slots, self.current_slots)
-        
+
         return suggest_slot_vals
-    
+
     def get_current_kb_results(self):
         """ get the kb_results for current state """
         kb_results = self.kb_helper.available_results_from_kb(self.current_slots)
         return kb_results
-    
-    
+
     def update(self, agent_action=None, user_action=None):
         """ Update the state based on the latest action """
 
         ########################################################################
         #  Make sure that the function was called properly
         ########################################################################
-        assert(not (user_action and agent_action))
-        assert(user_action or agent_action)
+        assert (not (user_action and agent_action))
+        assert (user_action or agent_action)
 
         ########################################################################
         #   Update state to reflect a new action by the agent.
         ########################################################################
         if agent_action:
-            
+
             ####################################################################
             #   Handles the act_slot response (with values needing to be filled)
             ####################################################################
             if agent_action['act_slot_response']:
                 response = copy.deepcopy(agent_action['act_slot_response'])
-                
-                inform_slots = self.kb_helper.fill_inform_slots(response['inform_slots'], self.current_slots) # TODO this doesn't actually work yet, remove this warning when kb_helper is functional
-                agent_action_values = {'turn': self.turn_count, 'speaker': "agent", 'diaact': response['diaact'], 'inform_slots': inform_slots, 'request_slots':response['request_slots']}
-                
-                agent_action['act_slot_response'].update({'diaact': response['diaact'], 'inform_slots': inform_slots, 'request_slots':response['request_slots'], 'turn':self.turn_count})
-                
+
+                inform_slots = self.kb_helper.fill_inform_slots(response['inform_slots'], self.current_slots)
+                # TODO this doesn't actually work yet, remove this warning when kb_helper is functional
+                agent_action_values = {'turn': self.turn_count, 'speaker': "agent", 'diaact': response['diaact'],
+                                       'inform_slots': inform_slots, 'request_slots': response['request_slots']}
+
+                agent_action['act_slot_response'].update({'diaact': response['diaact'], 'inform_slots': inform_slots,
+                                                          'request_slots': response['request_slots'],
+                                                          'turn': self.turn_count})
+
             elif agent_action['act_slot_value_response']:
                 agent_action_values = copy.deepcopy(agent_action['act_slot_value_response'])
                 # print("Updating state based on act_slot_value action from agent")
                 agent_action_values['turn'] = self.turn_count
                 agent_action_values['speaker'] = "agent"
-                
+
             ####################################################################
             #   This code should execute regardless of which kind of agent produced action
             ####################################################################
             for slot in agent_action_values['inform_slots'].keys():
                 self.current_slots['proposed_slots'][slot] = agent_action_values['inform_slots'][slot]
-                self.current_slots['inform_slots'][slot] = agent_action_values['inform_slots'][slot] # add into inform_slots
+                self.current_slots['inform_slots'][slot] = agent_action_values['inform_slots'][
+                    slot]  # add into inform_slots
                 if slot in self.current_slots['request_slots'].keys():
                     del self.current_slots['request_slots'][slot]
 
@@ -147,12 +150,12 @@ class StateTracker:
             self.history_dictionaries.append(agent_action_values)
             current_agent_vector = np.ones((1, self.action_dimension))
             self.history_vectors = np.vstack([self.history_vectors, current_agent_vector])
-                            
+
         ########################################################################
         #   Update the state to reflect a new action by the user
         ########################################################################
         elif user_action:
-            
+
             ####################################################################
             #   Update the current slots
             ####################################################################
@@ -164,9 +167,10 @@ class StateTracker:
             for slot in user_action['request_slots'].keys():
                 if slot not in self.current_slots['request_slots']:
                     self.current_slots['request_slots'][slot] = "UNK"
-            
-            self.history_vectors = np.vstack([self.history_vectors, np.zeros((1,self.action_dimension))])
-            new_move = {'turn': self.turn_count, 'speaker': "user", 'request_slots': user_action['request_slots'], 'inform_slots': user_action['inform_slots'], 'diaact': user_action['diaact']}
+
+            self.history_vectors = np.vstack([self.history_vectors, np.zeros((1, self.action_dimension))])
+            new_move = {'turn': self.turn_count, 'speaker': "user", 'request_slots': user_action['request_slots'],
+                        'inform_slots': user_action['inform_slots'], 'diaact': user_action['diaact']}
             self.history_dictionaries.append(copy.deepcopy(new_move))
 
         ########################################################################
